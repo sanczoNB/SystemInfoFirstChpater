c2921ed2d54ce99b6f31185abc8d35693f24e309
sanczoNB <sanczo16@gmail.com>
2017-01-07 18:55:01 +0100

    wyświetlanie podstawowych zmiennych środowiskowych

--- SystemInfo/Program.cs
+++ SystemInfo/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SystemInfo
 {
     class Program
     {
+        private static string system = "Informacje o systemie:"
+            +"\nWersja sytemu: " + Environment.OSVersion
+            +(Environment.Is64BitOperatingSystem ? ", 64 bitowy":"")
+            +"\nWersja Microsoft .NET Framework: " + Environment.Version
+            +"\nNazwa komputera: " + Environment.MachineName
+            +"\nKatalog systemowy: " + Environment.SystemDirectory
+            +"\nKatalog roboczy aplikacji: " + Environment.CurrentDirectory;
+
         static void Main(string[] args)
         {
+            Console.WriteLine("SystemInfo\n---------\n");
+            Console.WriteLine(system + "\n");
         }
+
+        
     }
 }
