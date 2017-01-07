2be50598ecfbfc487e69a43be28b7226994996df
sanczoNB <sanczo16@gmail.com>
2017-01-07 22:28:35 +0100

    dodanie wypisu informaji o użytkownikach

--- SystemInfo/Program.cs
+++ SystemInfo/Program.cs
@@ -4,7 +4,7 @@ namespace SystemInfo
 {
     class Program
     {
-        private static string system = "Informacje o systemie:"
+        private static readonly string System = "Informacje o systemie:"
             +"\nWersja sytemu: " + Environment.OSVersion
             +(Environment.Is64BitOperatingSystem ? ", 64 bitowy":"")
             +"\nWersja Microsoft .NET Framework: " + Environment.Version
@@ -12,10 +12,23 @@ namespace SystemInfo
             +"\nKatalog systemowy: " + Environment.SystemDirectory
             +"\nKatalog roboczy aplikacji: " + Environment.CurrentDirectory;
 
+        //Informacje o bieżącym użytkowniku
+        private static string user = "Informacje o bieżącym użytkowniku:"
+            +"\nNazwa użytkownika: " + Environment.UserName
+            +"\nKatalogi specjalne użytkownika:"
+            +"\nkatalog 'Moje dokumnety' = " + Environment.GetFolderPath(Environment.SpecialFolder.Personal)
+            +"\nkatalog ''Pulpit' = " + Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+            +"\nkatalog 'Moje obrazy' = " + Environment.GetFolderPath(Environment.SpecialFolder.MyPictures)
+            +"\nkatalog menu start = " + Environment.GetFolderPath(Environment.SpecialFolder.StartMenu)
+            +"\nkatalog 'Programy' = " + Environment.GetFolderPath(Environment.SpecialFolder.Programs)
+            +"\nkatalog 'Autostart' = " + Environment.GetFolderPath(Environment.SpecialFolder.Startup)
+            +"\nkatalog domowy użytkownika = " + Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
         static void Main(string[] args)
         {
             Console.WriteLine("SystemInfo\n---------\n");
-            Console.WriteLine(system + "\n");
+            Console.WriteLine(System + "\n");
+            Console.WriteLine(user + "\n");
         }
 
         
